ce94af440b2764181ad7374dacdadf80f52b0451
elliestewart98 <69251133+elliestewart98@users.noreply.github.com>
2020-08-17 20:21:27 +0100

    Completed 1 1 and 2
    Have created bounce.py and created new program called sears.py where I fixed the code.

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+# Write a program that prints a table showing 
+# the height of the first 10 bounces.
+# Round the output to four digits 
+initial_height = 100
+height = initial_height
+for i in range(9):
+	height = height * (3/5)
+	print(round(height,4))
