2ec00b67cc244e1ca56e95f0ad199a50cca25add
elliestewart98 <69251133+elliestewart98@users.noreply.github.com>
2020-08-22 13:38:32 +0100

    2 4 Portfolio Reader
    Reads in a file and creates a list of tuples

--- Work/pcost.py
+++ Work/pcost.py
@@ -6,7 +6,7 @@
 # purchase all of the shares in the portfolio.
 # Should print out: Total cost 44671.15
 import csv
-
+"""
 def portfolio_cost(filename):  
     with open(filename, 'rt') as f:
         #this reads in the first line and means you can skip the column headers
@@ -27,7 +27,21 @@ def portfolio_cost(filename):
         for i in range(len(shares)):
             share_prices.append(shares[i]*prices[i])
         return(sum(share_prices))
-        
+"""       
+#Given solution
+def portfolio_cost(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    total_cost = 0.0
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            nshares = int(row[1])
+            price = float(row[2])
+            total_cost += nshares * price
+    return total_cost
+
 
 cost = portfolio_cost('Data/portfolio.csv')
 print('Total cost:', cost)
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,24 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+#opens a given portfolio file and reads it into a list of tuples
+def read_portfolio(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    portfolio = []
+    
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+    return portfolio
+
+
+portfolio = read_portfolio('Data/portfolio.csv')
+total = 0.0
+for name,shares,price in portfolio:
+    total += shares *price
+print(total)
