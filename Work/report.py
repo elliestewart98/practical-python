8d21f50371a3966ca0ef728258b4a7396574fc4a
elliestewart98 <69251133+elliestewart98@users.noreply.github.com>
2020-08-22 14:05:34 +0100

    2 5 Reads in file
    Reads in  file and creates list of dictionaries

--- Work/report.py
+++ Work/report.py
@@ -2,7 +2,7 @@
 #
 # Exercise 2.4
 import csv
-
+"""
 #opens a given portfolio file and reads it into a list of tuples
 def read_portfolio(filename):
     '''Computes the total cost (shares*price) of a portfolio file'''
@@ -15,10 +15,37 @@ def read_portfolio(filename):
             holding = (row[0], int(row[1]), float(row[2]))
             portfolio.append(holding)
     return portfolio
-
-
 portfolio = read_portfolio('Data/portfolio.csv')
 total = 0.0
 for name,shares,price in portfolio:
     total += shares *price
 print(total)
+"""
+
+#opens a given portfolio file and reads it into a list of dictionaries
+def read_portfolio(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    portfolio = []
+    
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            portfolio_dict = {}
+            portfolio_dict[headers[0]] = row[0]
+            portfolio_dict[headers[1]] = int(row[1])
+            portfolio_dict[headers[2]] = float(row[2])
+            portfolio.append(portfolio_dict)
+    return portfolio
+
+portfolio = read_portfolio('Data/portfolio.csv')
+print(portfolio)
+print(portfolio[1]['shares'])
+total = 0.0
+for s in portfolio:
+    total += s['shares']*s['price']
+
+print(total)
+
+from pprint import pprint
+pprint(portfolio)
