8fc4eab3b9bdfa9c64ff731b1d1bd5882d8a6198
elliestewart98 <69251133+elliestewart98@users.noreply.github.com>
2020-08-23 11:37:01 +0100

    2 10 Print a formatted table
    This places the statements in a function and shows you how to format a table by alignning the decimal points in a way that evenly distributes the columns

--- Work/report.py
+++ Work/report.py
@@ -99,10 +99,36 @@ for stock in portfolio:
     profit = share * difference
     print("Name: " + name + " Current value: " + str(current_value)
           +" Gain/loss: " + str(profit))
-    
 
+# Write a function make_report() that takes a list of stocks
+# and dictionary of prices as input and returns a list of 
+# tuples containing the rows of the above table.   
+def make_report(portfolio, prices):
+    list_of_rows = [] 
+    for stock in portfolio:
+        name = stock['name']
+        share = stock['shares']
+        initial_price = stock['price']
+        current_price = prices[name]
+        difference = current_price - initial_price
+        current_value = share * current_price
+        profit = share * difference
+        holding = (name,share, current_price, difference)
+        list_of_rows.append(holding)
+        print("Name: " + name + " Current value: " + str(current_value)
+              +" Gain/loss: " + str(profit))
+    return list_of_rows
 
-
+report = make_report(portfolio,prices)
+# This formats the report as a table by formatting the tuples
+"""
+for r in report:
+    print('%10s %10d %10.2f %20.2f' % r)
+"""   
+# Or you can expand the values and use fstrings
+# This aligns them all evenly by decimal point
+for name, shares, price, change in report:
+    print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
 
 
 
