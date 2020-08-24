f6aeaab921f73961f26379d5a23a044d9842ee6e
elliestewart98 <69251133+elliestewart98@users.noreply.github.com>
2020-08-24 11:29:12 +0100

    3 8 RuntimeError
    Raise an exception when you select headers but also declare there to be no headers in the parsecsv function

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -181,10 +181,56 @@ prices = parse_csv_4('Data/prices.csv', types=[str,float],
 print('\n', prices)
 
 
+# Modify the code so that an exception gets raised if both 
+# the select and has headers arguments are passed
 
+def parse_csv_5(filename, select=None, types=None, has_headers=True, delimiter=','):
+    '''
+    Parse a CSV file into a list of records with type conversion.
+    '''
+    with open(filename) as f:
+        rows = csv.reader(f, delimiter=delimiter)
 
+        if select and (has_headers == False):
+            raise RuntimeError('Cannot select headers if there are none.')
+        
+        # Read the file headers (if any)
+        headers = next(rows) if has_headers else []
 
+        # If specific columns have been selected, make indices for filtering 
+        if select:
+            indices = [ headers.index(colname) for colname in select ]
+            headers = select
 
+        records = []
+        for row in rows:
+            if not row:     # Skip rows with no data
+                continue
+
+            # If specific column indices are selected, pick them out
+            if select:
+                row = [ row[index] for index in indices]
+
+            # Apply type conversion to the row
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
+
+            # Make a dictionary or a tuple
+            if headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+            records.append(record)
+
+        return records
+
+prices_fail = parse_csv_5('Data/prices.csv',
+                        select=['name','price'],
+                        has_headers=False)
+
+# Modify the function to catch all ValueError exceptions
+# generated during record creation and print a warning
+# message for rows that cant be converted. 
 
 
 
